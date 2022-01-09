4df042150a9a6b82339780cc5fa03ff737f3835b
malikrawashdeh <malikrawashdeh5@gmail.com>
2022-01-08 21:41:13 -0600

    uploading to github

---
+++ src/api/index.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableUrl);
+    const modifiedData = {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(
+      "https://api.covidtracking.com/v1/us/daily.json"
+    );
+    // console.log(data);
+    return data.map(({ positive, recovered, death, dateChecked: date }) => ({
+      confirmed: positive,
+      recovered,
+      deaths: death,
+      date,
+    }));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {}
+};
